a0b1585205acfbc7f759280374ff86a595b4916a
Andrei Bissolotti <andreibisso@gmail.com>
2021-10-04 09:24:06 -0300

    Adiciona eventos ao component

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './tic-tac-toe.component.html',
   styleUrls: ['./tic-tac-toe.component.css']
 })
-export class TicTacToeComponent implements OnInit {
+export class TicTacToeComponent {
 
-  constructor() { }
+  currentPlayer: string = "X";
+  winner: string = '';
+  bord: string[][] = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+  ];
 
-  ngOnInit(): void {
+  processPlay(i: number, j: number){
+    console.log(`Jogada na linha ${i}, coluna ${j} do jogador ${this.currentPlayer}`)
   }
+
+
+  constructor () { }
 
 }
